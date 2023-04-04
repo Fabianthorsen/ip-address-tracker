410ef0266ffa6a802dfd776d384fa7bc3a9ab010
Fabian Thorsen <thorsen.fabian@gmail.com>
2023-04-04 18:57:46 +0200

    adding deps and change minor to code

--- src/netlify/functions/fetch-ip.ts
+++ src/netlify/functions/fetch-ip.ts
@@ -1,9 +1,12 @@
-exports.handler = async function(event, context) {
+import { HandlerEvent } from "@netlify/functions";
+
+exports.handler = async function(event: HandlerEvent) {
     try {
-        const {ip} = event.queryStringParameter;
+        const params = event.queryStringParameters;
         const API = process.env.VITE_API_KEY;
 
-        const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${API}&ipAddress=${ip}`
+        const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${API}&ipAddress=${params!.ip}`
+
 
         const response = await fetch(url);
         const data = await response.json();
@@ -20,24 +23,3 @@ exports.handler = async function(event, context) {
         }
     }
 }
-
-  async function fetchData(event: FormEvent) {
-    event.preventDefault();
-    const resp = await fetch(
-      `https://geo.ipify.org/api/v2/country,city?apiKey=${
-        import.meta.env.VITE_API_KEY
-      }&ipAddress=${inputIp}`
-    );
-    const data = await resp.json();
-    const geoData: GeoData = {
-      ipAddress: data.ip,
-      location: data.location.city,
-      timezone: "UTC " + data.location.timezone,
-      coordinates: {
-        lat: Number(data.location.lat),
-        lng: Number(data.location.lng),
-      },
-      isp: data.isp,
-    };
-    setState(geoData);
-  }
